22d33d33e3f8ae8661de397e3a86ee4e85e97c56
Shantanu <kumar.shantanu@accoliteindia.com>
2019-01-21 17:40:37 +0530

    Project

---
+++ MultiThreading/TaxiManagement/src/au/accolite/assignment/taximanagnment/Passenger.java
@@ -0,0 +1,19 @@
+package au.accolite.assignment.taximanagnment;
+
+public class Passenger {
+	public static enum DESTINATION {
+        WHITEFIELD, SARJAPUR, KRPURAM, INDIRANAGAR, MGROAD, HEBBAL, JAYANAGAR, PEENYA, HSRLAYOUT, KORAMANGLA
+    }
+    public static final DESTINATION[] DESTINATION_VALUES = DESTINATION.values();
+
+
+    private DESTINATION destination;
+
+    public Passenger(DESTINATION dest){
+        destination = dest;
+    }
+    public DESTINATION getDestination() {
+        return destination;
+    }
+
+}
