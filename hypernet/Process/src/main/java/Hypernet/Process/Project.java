8c496965147081e4bd88aa51a7d774f2d4eb28fb
Shantanu <kumar.shantanu@accoliteindia.com>
2019-01-22 17:07:27 +0530

    Project

---
+++ hypernet/Process/src/main/java/Hypernet/Process/Project.java
@@ -0,0 +1,57 @@
+package Hypernet.Process;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+@Entity
+@Table(name="projects")
+public class Project {
+	@Id
+	@Column(name="projectId")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Integer projectId;
+
+	@Column(name="projectName")
+	public String projectName;
+
+	@Column(name="employeeId")
+	public Integer employeeId;
+
+
+	
+	public Integer getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(Integer projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	
+}
